0e41bd75af8c1985c7788e3ea8c0fb6a5b5a082a
Majd Aburas <1aburasmaj@hdsb.ca>
2023-02-01 00:41:26 -0500

    Completed minor house keeping tasks, prepared for next phase. (#1)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -46,10 +46,13 @@ public class Constants {
         public static final int VERSION = 0;
         public static final String TEACH_MODE_FILE_NAME = "LIVE_RECORD";
 
+        // Auto directory
+        public static final String ROOT_DIRECTORY = "csv/";
+
         // Subsytems toggle logic
         public static final boolean DRIVE_ENABLED = true;
-        public static final boolean ARM_ENABLED = true;
-        public static final boolean CLAW_ENABLED = true;
+        public static final boolean ARM_ENABLED = false;
+        public static final boolean CLAW_ENABLED = false;
         public static final boolean MINI_BOT = false;
 
         // Drive PID Constants
@@ -59,15 +62,12 @@ public class Constants {
         public static final SparkMaxConstants DRIVER1_CONSTANTS = new SparkMaxConstants(1e-4, 0, 0, 0, 0.000156, -1, 1,
                         0,
                         0, 6000, 3500, 0.1);
-        public static final SparkMaxConstants DRIVEL2_CONSTANTS = new SparkMaxConstants(1e-4, 0, 0, 0, 0.000156, -1, 1,
-                        0,
-                        0, 6000, 3500, 0.1);
-        public static final SparkMaxConstants DRIVER2_CONSTANTS = new SparkMaxConstants(1e-4, 0, 0, 0, 0.000156, -1, 1,
-                        0,
-                        0, 6000, 3500, 0.1);
 
         // Gyro PID Constants
         public static final Terms GYRO_CONSTANTS = new Terms(0.005, 0.001, 0, 0);
+
+        // Target seek PID Constants
+        public static final Terms SEEK_CONSTANTS = new Terms(0.0, 0.0, 0, 0);
 
         // Arm PID Constants
         public static final SparkMaxConstants ARM_CONSTANTS = new SparkMaxConstants(1e-4, 0, 0, 0, 0.000156, -1, 1,
@@ -83,7 +83,4 @@ public class Constants {
         public static final SparkMaxConstants CLAW_CONSTANTS = new SparkMaxConstants(1e-4, 0, 0, 0, 0.000156, -1, 1,
                         0,
                         0, 4000, 2500, 0.1);
-
-        // Auto directory
-        public static final String ROOT_DIRECTORY = "csv/";
 }
--- src/main/java/frc/teleop/TeleopDriver.java
+++ src/main/java/frc/teleop/TeleopDriver.java
@@ -2,22 +2,15 @@ package frc.teleop;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.io.DriverInput;
-import frc.robot.Constants;
 import frc.subsystems.Drivetrain;
-import frc.util.devices.Controller;
-import frc.util.devices.Gyro;
-import frc.util.devices.Controller.Axis;
-import frc.util.devices.Controller.Side;
 
 public class TeleopDriver extends TeleopComponent {
     private static TeleopDriver instance;
 
     private Drivetrain drivetrain;
-
-    private Controller driverController;
     private DriverInput driverInput;
 
-    private double straight = 0;
+    private double frwd = 0;
     private double turn = 0;
 
     /**
@@ -35,43 +28,31 @@ public class TeleopDriver extends TeleopComponent {
     private TeleopDriver() {
         this.driverInput = DriverInput.getInstance();
         this.drivetrain = Drivetrain.getInstance();
-        this.driverController = driverInput.getDriverController();
     }
 
     @Override
     public void firstCycle() {
         this.drivetrain.firstCycle();
-        SmartDashboard.putNumber("Intake Motor Speed", 0.8);
     }
 
     @Override
     public void run() {
 
-        double speed = Constants.MAX_OUTPUT;
+        SmartDashboard.putNumber("Max Drive Speed %", this.driverInput.getDriveMaxSpeed() * 100);
 
-        speed -= (this.driverController.getLeftTriggerAxis() * Constants.MAX_INTERVAL);
-        speed += (this.driverController.getRightTriggerAxis() * Constants.MAX_INTERVAL);
+        if (!this.drivetrain.getPositionMode())
+            this.drivetrain.setBrakes(this.driverInput.getDriveBrakes());
 
-        SmartDashboard.putNumber("Drive Output", speed);
-
-        this.straight = this.driverController.getJoystick(Side.LEFT, Axis.Y) * speed;
-        this.turn = this.driverController.getJoystick(Side.RIGHT, Axis.X) * speed;
-
-        if (!this.drivetrain.getPositionMode()) {
-            if (this.driverController.getLeftBumper())
-                this.drivetrain.brake(true);
-            else
-                this.drivetrain.brake(false);
+        if (this.driverInput.getBalanceMode()) {
+            this.frwd = this.drivetrain.balance();
+            this.turn = 0;
+        } else {
+            this.drivetrain.balanceIdle();
+            this.frwd = this.driverInput.getDriveFrwd();
+            this.turn = this.driverInput.getDriveTurn();
         }
 
-        if (this.driverController.getAButton()) {
-            this.drivetrain.brake(true);
-            this.straight = this.drivetrain.balance();
-            this.turn = 0;
-        } else
-            this.drivetrain.balanceIdle();
-
-        this.drivetrain.setOutput(straight, turn);
+        this.drivetrain.setOutput(frwd, turn);
         this.drivetrain.run();
 
     }
