747bf76494cc203ad0353f665bd990fdd4ba509a
PhillipKim123456 <59770574+PhillipKim123456@users.noreply.github.com>
2023-02-07 15:51:31 -0500

    limelight/apriltag code

---
+++ src/main/java/frc/util/imaging/LimelightIO.java
@@ -0,0 +1,298 @@
+package frc.util.imaging;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+//import frc.robot.Constants;
+import java.lang.Object;
+import java.awt.geom.Line2D;
+import java.util.*;
+
+import frc.subsystems.Drivetrain;
+//import frc.io.SensorInput; //for future sensor input (motors and gyro)
+
+public class LimelightIO {
+    private static LimelightIO Instance;
+
+    private NetworkTable table;
+
+    private Drivetrain dr = Drivetrain.getInstance();
+
+    private NetworkTableEntry tx; //x axis angle from target
+    private NetworkTableEntry ty; //y axis angle from target
+    private NetworkTableEntry ta; //area of target
+    private NetworkTableEntry tid; //id of target
+    private NetworkTableEntry tv; //if there is a target
+    private NetworkTableEntry getpipe; //get current pipeline for limelight
+    private NetworkTableEntry camerapose; //camera position in the game field
+    private NetworkTableEntry botpose; //bot position in the game field
+
+    public static LimelightIO getInstance(){
+        if(Instance==null){
+            Instance = new LimelightIO();
+        }
+        return Instance;
+    }
+
+    public LimelightIO(){
+        table = NetworkTableInstance.getDefault().getTable("limelight");
+
+        tx = table.getEntry("tx");
+        ty = table.getEntry("ty");
+        ta = table.getEntry("ta");
+        botpose = table.getEntry("botpose_targetspace");
+        tid = table.getEntry("tid");
+        tv = table.getEntry("tv");
+        getpipe = table.getEntry("getpipe");
+        camerapose = table.getEntry("camerapose_targetspace");
+    }
+
+    public void setLedMode(int ledMode) { //0 = set to pipeline led state, 1 = set to off,
+                                          //2 = set to blink, 3 = set to constant on
+        this.table.getEntry("ledMode").setNumber(ledMode);
+    }
+    public double gettid(){
+        return tid.getDouble(0.0);
+    }
+    public boolean gettv(){
+        return (tv.getDouble(0.0)==0?false:true);
+    }
+    public double getpipe(){
+        return getpipe.getDouble(0.0);
+    }
+    public double[] getcamerapose(){
+        return camerapose.getDoubleArray(new double[6]);
+    }
+
+    public double getdistancevert(){ //get vertical distance from bot to april tag
+        //-1=place holder
+        double h1 = 37; //height of april tag from ground
+        double h2 = -1; //height of limelight from ground
+
+        double a1 = getty(); //angle of limelight crosshair to april tag
+        double a2 = -1; //angle of limelight on robot vertically
+        double ar = (a1+a2)*(Math.PI/180); //merge both angles and turn to radians
+        double dis = (h1-h2)/Math.tan(ar); //merge lengths and apply trig: tan(a)=opposite/adjacent
+
+        dis = Math.round(dis*100)/100.0;
+
+        return dis;
+    }
+
+    public double getdisthor(){ //get horizontal distance from bot to april tag 
+        //-1=place holder
+        double h1 = getdistancevert(); //vertical distance from bot to april tag
+        double h2 = -1; //how far back the lime light is from the front of the bot
+
+        double a1 = gettx(); //the angle from the bot to the april tag horizontally
+        double a2 = -1; //angle of limelight on robot horizontally
+        double ar = (a1+a2)*(Math.PI/180); //merge both angles and turn to radians
+        double dis = (h1-h2)*Math.tan(ar); //merge lengths and apply trig: tan(a)=opposite/adjacent
+
+        dis = Math.round(dis*100)/100.0;
+        return dis;
+    }
+
+    private class pair{
+        double first;
+        double second;
+        private pair(double f, double s){
+            first=f;
+            second=s;
+        }
+    }
+
+    private pair coords[]; //array with coords for differing stations
+
+    public void gotostat(int id){ //calls gotocoords using the preset coords of each respective station
+        gotocoords(coords[id].first, coords[id].second);
+    }
+
+    private double getdis(double x1, double y1, double x2, double y2){
+        return Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+    }
+
+    public void gotocoords(double x, double y){ //actual method that makes bot go vroom vroom (in progress)
+        double curx = getx();
+        double cury = gety();
+        double curAng = getanglex();
+
+        double s = 0;
+
+        double[] result = crosscharge(curx, cury, x-s, y);
+        while(result[0]!=Double.MAX_VALUE){//continuosly move until charge station is no longer in way of path
+            if(getdis(x, y, result[0], result[1])>getdis(x, y, result[2], result[3])){ //move to corner of charge station closest to original target
+                //drive to result[0] and result[1] may need tuning
+                //turn to target position
+                double ang = Math.atan2(result[0]-curx, result[1]-cury);
+                dr.seekTargetPID(getanglex()-ang); //method doesnt exist in this branch
+                //move forward until at target
+
+
+                curx=result[0];
+                cury=result[1];
+            }else{
+                double ang = Math.atan2(result[0]-curx, result[1]-cury);
+                //drive to result[2] and result[3] may need tuning
+                dr.seekTargetPID(getanglex()-ang); //method doesnt exist in this branch
+                //move forward until at target
+                curx=result[2];
+                cury=result[3];
+            }
+
+            result = crosscharge(curx, cury, x-s, y);
+        }
+
+        //clear path, drive directly to coords but leave some space
+        //use space to rotate bot towards target
+        //move forward to get to target coords
+
+    } 
+    
+    
+
+    //temp coords of charge station
+    private double cx1=0;//x coord of top right of charge station
+    private double cy1=0;//y coord of top right of charge station
+    private double cx2=0;//x coord of bottom left of charge station
+    private double cy2=0;//y coord of bottom left of charge station
+
+    public double[] crosscharge(double x1, double y1, double x2, double y2){ //x1 and y1 is bot pose, x2 and y2 is target pose
+        //line of charging station which bot crosses, in the form of (x1, y1), (x2, y2)
+        double[] result = new double[4];
+        Arrays.fill(result, Double.MAX_VALUE); //if all values within result are Double.Max_Value once returned then the path does not cut across charge station
+
+        //declaring line segments for the sides of the charging station and the robot path
+        Line2D route = new Line2D.Double(x1, y1, x2, y2); //robot path
+        Line2D top = new Line2D.Double(cx2, cy1, cx1, cy1); //top charging station line segment
+        Line2D btm = new Line2D.Double(cx2, cy2, cx1, cy2); //bottom charging station line segment
+        Line2D left = new Line2D.Double(cx2, cy2, cx2, cy1); //left charging station line segment
+        Line2D right = new Line2D.Double(cx1, cy2, cx1, cy1); //right charging station line segment
+
+        if(y2-y1==0){ //if path is parallel to x axis
+            if(x2>x1&&route.intersectsLine(left)){//if target pose is to the right of bot
+                setresult(result, "left");
+            }else if(route.intersectsLine(right)){//if target is to the left of bot
+                setresult(result, "right");
+            }
+            return result;
+        }
+        if(x2-x1==0){ //if path is perpendicular to x axis
+            if(y2>y1&&route.intersectsLine(top)){//if target is above the bot
+                setresult(result, "top");
+            }else if(route.intersectsLine(btm)){//if target is below the bot
+                setresult(result, "btm");
+            }
+            return result;
+        }
+        //models the path from current location (x1, y1) to the destination (x2, y2) as a function y=mx+b
+        double mx = (y2-y1)/(x2-x1); //get slope value of function
+
+        if(mx>0){
+            //slope is positive
+            if(x2>x1){
+                //x increases and slope is positive, meaning robot goes from btm left to top right, thus must cross btm or left line first
+                if(route.intersectsLine(left)){
+                    setresult(result, "left");
+                }else if(route.intersectsLine(btm)){
+                    setresult(result, "btm");
+                }
+                return result; 
+            }else {
+                //x decreases and slope is positive, meaning robot goes from top right to btm left, thus must cross top or right line first
+                if(route.intersectsLine(top)){
+                    setresult(result, "top");
+                }else if(route.intersectsLine(right)){
+                    setresult(result, "right");
+                }
+                return result;
+            }
+        }else if(mx<0){
+            //slope is negative
+            if(x2>x1){
+                //x increases and slope is negative, meaning robot goes from top left to btm right, thus must cross top or left line first
+                if(route.intersectsLine(top)){
+                    setresult(result, "top");
+                }else if(route.intersectsLine(left)){
+                    setresult(result, "left");
+                }
+                return result;
+            }else {
+                //x decreases and slope is negative, meaning robot goes from btm right to top left, thus must cross btm or right line first
+                if(route.intersectsLine(btm)){
+                    setresult(result, "btm");
+                }else if(route.intersectsLine(right)){
+                    setresult(result, "right");
+                }
+                return result;
+            }
+        }
+        return result;
+    }
+
+    private void setresult(double result[], String s){//function to set values for intersection
+        if(s.equals("left")){
+            result[0] = cx2;
+            result[1] = cy2;
+            result[2] = cx2;
+            result[3] = cy1;
+        }else if(s.equals("right")){
+            result[0] = cx1;
+            result[1] = cy2;
+            result[2] = cx1;
+            result[3] = cy1;
+        }else if(s.equals("top")){
+            result[0] = cx2;
+            result[1] = cy1;
+            result[2] = cx1;
+            result[3] = cy1; 
+        }else{ //bottom
+            result[0] = cx2;
+            result[1] = cy2;
+            result[2] = cx1;
+            result[3] = cy2;
+        }
+    }
+
+    public void drive(double x){
+        //go forward a certain amount using spark max metre sensors
+    }
+
+    public void turnangle(double angle){
+
+        //algorithm to change angle of bot goes here
+
+    }
+
+    public double gettx(){
+        return tx.getDouble(0.0);
+    }
+
+    public double getty(){
+        return ty.getDouble(0.0);
+    }
+
+    public double getta(){
+        return ta.getDouble(0.0);
+    }
+
+    public double getx(){
+        return botpose.getDoubleArray(new double[6])[0];
+    }
+
+    public double gety(){
+        return botpose.getDoubleArray(new double[6])[1];
+    }
+
+    public double getz(){
+        return botpose.getDoubleArray(new double[6])[2];
+    }
+
+    public double getanglex(){
+        return botpose.getDoubleArray(new double[6])[3];
+    }
+
+    public double[] getbotposearr(){
+        return botpose.getDoubleArray(new double[6]);
+    }
+}
